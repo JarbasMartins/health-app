6db3f34729cabcb7370d1878c3e59edcce027385
Jarbas Martins <jarbasmartins0708@gmail.com>
2025-12-06 20:20:05 -0300

    update schema validators

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,22 +1,29 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const registerSchema = z
-    .object({
-        name: z.string().min(5).max(100),
-        email: z.email(),
-        password: z.string().min(8),
-        confirmPassword: z.string(),
-    })
-    .refine((data) => data.password === data.confirmPassword, {
-        message: "Senhas não coicidem.",
-        path: ["confirmPassword"],
-    });
-
-export type RegisterType = z.infer<typeof registerSchema>;
+  .object({
+    name: z.string().min(5).max(100),
+    email: z.email(),
+    password: z.string().min(8),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Senhas não coicidem.',
+    path: ['confirmPassword'],
+  });
 
 export const loginSchema = z.object({
-    email: z.string(),
-    password: z.string(),
+  email: z.string(),
+  password: z.string(),
 });
 
+export const medicationSchema = z.object({
+  name: z.string().min(1),
+  time: z.string().min(1),
+  quantity: z.string().min(1),
+  description: z.string().optional(),
+});
+
+export type RegisterType = z.infer<typeof registerSchema>;
 export type LoginType = z.infer<typeof loginSchema>;
+export type MedicationType = z.infer<typeof medicationSchema>;
