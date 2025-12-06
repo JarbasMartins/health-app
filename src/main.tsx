b59cb50ccd3c1ab87fbf3bf9aa0c3196d77e16bc
Jarbas Martins <jarbasmartins0708@gmail.com>
2025-12-06 20:21:11 -0300

    chore: misc updates and small fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/routes';
+import AppShell from './appShell';
 
-import "./index.css";
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AppShell>
+      <RouterProvider router={router} />
+    </AppShell>
+  </React.StrictMode>
 );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,34 +1,23 @@
-import { createBrowserRouter } from "react-router-dom";
-import { UserProvider } from "../context/UserContext";
-import RegisterPage from "../pages/auth/Register";
-import LoginPage from "../pages/auth/Login";
-import HomePage from "../pages/Home";
-import { Outlet } from "react-router-dom";
-
-function Layout() {
-    return (
-        <UserProvider>
-            <Outlet />
-        </UserProvider>
-    );
-}
+import { createBrowserRouter } from 'react-router-dom';
+import RegisterPage from '../pages/Register';
+import LoginPage from '../pages/Login';
+import HomePage from '../pages/Home';
 
 export const router = createBrowserRouter([
-    {
-        element: <Layout />,
-        children: [
-            {
-                path: "/login",
-                element: <LoginPage />,
-            },
-            {
-                path: "/register",
-                element: <RegisterPage />,
-            },
-            {
-                path: "/",
-                element: <HomePage />,
-            },
-        ],
-    },
+  {
+    children: [
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/register',
+        element: <RegisterPage />,
+      },
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+    ],
+  },
 ]);
